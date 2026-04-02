76ae4f1ca073c06a8382591f32a64eb2e7b53dea
oren-hari <88973831+orenefrontier@users.noreply.github.com>
2026-04-02 07:42:35 -0300

    rename project

--- target/debug/deps/hello_world-cf3d9fb3682ca0d2.d
+++
@@ -1,5 +0,0 @@
-/Users/oren/Documents/personal/work_preparations/NOEON/mirrors_and_Lasers/target/debug/deps/hello_world-cf3d9fb3682ca0d2.d: src/main.rs
-
-/Users/oren/Documents/personal/work_preparations/NOEON/mirrors_and_Lasers/target/debug/deps/libhello_world-cf3d9fb3682ca0d2.rmeta: src/main.rs
-
-src/main.rs:
---
+++ target/debug/deps/mirrors_and_lasers-6de516bec4258346.d
@@ -0,0 +1,6 @@
+/Users/oren/Documents/personal/work_preparations/NOEON/mirrors_and_Lasers/target/debug/deps/mirrors_and_lasers-6de516bec4258346.d: src/main.rs src/solver.rs
+
+/Users/oren/Documents/personal/work_preparations/NOEON/mirrors_and_Lasers/target/debug/deps/libmirrors_and_lasers-6de516bec4258346.rmeta: src/main.rs src/solver.rs
+
+src/main.rs:
+src/solver.rs:
